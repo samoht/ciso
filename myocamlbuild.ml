d13bc42017d77a8e234da5199643bdc9f975f7ac
Thomas Gazagnaire <thomas@gazagnaire.org>
2016-02-11 00:03:40 +0000

    fix build for 4.01

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -632,8 +632,10 @@ let dispatch_default = MyOCamlbuildBase.dispatch_default conf package_default;;
 # 633 "myocamlbuild.ml"
 (* OASIS_STOP *)
 Ocamlbuild_plugin.dispatch dispatch_default;;
+(*
 Ocamlbuild_plugin.mark_tag_used "tests";;
 Ocamlbuild_plugin.mark_tag_used "pkg_github.unix";;
+*)
 let () =
   flag ["ocaml"; "doc"] (A"-colorize-code");
   flag ["ocaml"; "doc"] (A"-short-functors");
